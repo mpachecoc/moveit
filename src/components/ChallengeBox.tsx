ea984e0d0fa3c3e5c79b0f5169ee3f57c0fc8f50
Mauricio Pacheco <mauriciopachecoc@gmail.com>
2021-02-25 07:56:13 -0400

    Created ChallengeContext

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,24 +1,28 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
-  const hasActiveChallenge = true;
+  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
 
   return (
     <div className={styles.challengeBoxContainer}>
-      {hasActiveChallenge ? (
+      {activeChallenge ? (
         <div className={styles.challengeActive}>
-          <header>Win 400 xp</header>
+          <header>Win {activeChallenge.amount} xp</header>
 
           <main>
-            <img src="icons/body.svg" alt=""/>
+            <img src={`icons/${activeChallenge.type}.svg`} alt=""/>
             <strong>New Challenge</strong>
-            <p>Stand up and make a 3 minute walk.</p>
+            <p>{activeChallenge.description}</p>
           </main>
 
           <footer>
             <button 
               type="button" 
               className={styles.challengeFailButton}
+              onClick={resetChallenge}
             >
               Failed
             </button>
